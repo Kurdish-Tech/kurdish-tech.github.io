2b3f693b166f4c2355a38f0a3e7dc1a0846724c2
alanhasn <dlbrinhsn1122@gmail.com>
2026-07-02 09:09:57 -0800

    config: update repository name and set base url to root

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // تم التعديل إلى '/' لأن الموقع أصبح على النطاق الرئيسي مباشرة
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
